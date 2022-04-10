faae62933e78de2761e2ecba56550a60d51f28fd
trinhnv <trinhnguyen959@gmail.com>
2022-04-10 23:12:14 +0700

    get element method 1

--- core-js/dom/basics/index.js
+++ core-js/dom/basics/index.js
@@ -1,6 +1,42 @@
 /*
-* HTML DOM
-* 1. Element
-* 2. Attribute
-* 3. Text
-* */
+ * HTML DOM
+ * 1. Element
+ *  - Id
+ *  - class
+ *  - tag
+ *  - css selector
+ *  - html collection
+ * 2. Attribute
+ * 3. Text
+ * */
+
+// id -> object
+let headingId = document.getElementById('heading');
+console.log('getElementById', { headingId });
+
+// class -> html collection
+let headingClasses = document.getElementsByClassName('heading');
+console.log('getElementsByClassName', headingClasses);
+
+// tag -> html collection
+let headingTag = document.getElementsByTagName('h1');
+console.log('heading tag', headingTag);
+
+// css selector -> object
+let headingCss = document.querySelector('.heading');
+console.log('querySelector', { headingCss });
+let heading2Css = document.querySelector('.box .heading-2');
+console.log({ heading2Css });
+// node list
+let headingNodeList = document.querySelectorAll('.heading');
+console.log('querySelectorAll', headingNodeList);
+
+// form
+let firstForm = document.forms['form-1'];
+console.log('1st way query form', { firstForm });
+let thirdForm = document.forms.thirdForm;
+console.log('2st way query form', { thirdForm });
+
+// anchors -> Deprecated
+let anchors = document.anchors;
+console.log(anchors);
