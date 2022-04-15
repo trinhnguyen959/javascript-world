98ed444511dd0694bd3d5a3691cfd11a9150c8b3
trinhnv <trinhnguyen959@gmail.com>
2022-04-15 14:59:40 +0700

    add json server courses

--- core-js/json-fetch/json-server/index.js
+++ core-js/json-fetch/json-server/index.js
@@ -1,1 +1,74 @@
 // json server
+let coursesApi = 'http://localhost:3000/courses';
+
+// start
+let start = () => {
+	getCourses(courses => {
+		renderCourse(courses);
+
+		handleCreateForm();
+	});
+};
+
+start();
+
+// functions
+function getCourses(callback) {
+	fetch(coursesApi)
+		.then(res => res.json())
+		.then(callback);
+}
+
+function renderCourse(courses) {
+	let listCourse = document.querySelector('#courses');
+	let htmlMap = courses.map(
+		course => `<li class="item-${course.id}">
+				<h4>${course.name}</h4>
+				<p>${course.description}</p>
+				<button onclick="handleDeleteCourse(${course.id})">Delete</button>
+			</li>`
+	);
+	listCourse.innerHTML = htmlMap.join('');
+}
+
+function createCourse(data, callback) {
+	let options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	};
+	fetch(coursesApi, options)
+		.then(res => res.json())
+		.then(callback);
+}
+
+function handleDeleteCourse(courseId) {
+	let options = {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	fetch(coursesApi + '/' + courseId, options)
+		.then(res => res.json())
+		.then(() => {
+			let courseDeleted = document.querySelector('.item-' + courseId);
+			if (courseDeleted) {
+				courseDeleted.remove();
+			}
+		});
+}
+
+function handleCreateForm() {
+	let createBtn = document.querySelector('#createBtn');
+	createBtn.onclick = () => {
+		let name = document.querySelector('input[name="name"]').value;
+		let description = document.querySelector(
+			'input[name="description"]'
+		).value;
+
+		createCourse({ name, description }, () => getCourses(renderCourse));
+	};
+}
