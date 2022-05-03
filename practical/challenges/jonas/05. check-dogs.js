9814c9d57093073174b623c4a69d591a673eb4a5
trinhnv <trinhnguyen959@gmail.com>
2022-05-04 00:23:47 +0700

    add challenge micro optimize

--- practical/challenges/jonas/05. check-dogs.js
+++ practical/challenges/jonas/05. check-dogs.js
@@ -60,11 +60,22 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const calcAverageHumanAge = ages =>
-	ages
-		.map(age => (age > 2 ? 16 + age * 4 : age * 2))
-		.filter(age => age >= 18)
-		.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+const calcAverageHumanAge = ages => {
+	const calAge = age => (age > 2 ? 16 + age * 4 : age * 2);
+	return (
+		ages
+			// .map(age => (age > 2 ? 16 + age * 4 : age * 2))
+			// .filter(age => age >= 18)
+			.reduce((result, age) => {
+				const finalAge = calAge(age);
+				if (finalAge >= 18) {
+					result.push(finalAge);
+				}
+				return result;
+			}, [])
+			.reduce((acc, age, i, arr) => acc + age / arr.length, 0)
+	);
+};
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
