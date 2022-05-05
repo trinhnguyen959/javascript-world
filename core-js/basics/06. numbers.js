eb67698ba398d160680501579f5a50394e4486c1
trinhnv <trinhnguyen959@gmail.com>
2022-05-05 21:08:44 +0700

    update number

--- core-js/basics/06. numbers.js
+++ core-js/basics/06. numbers.js
@@ -19,3 +19,37 @@ console.log('PI to fix:', PI.toFixed());
 console.log('abovePI to fix:', abovePI.toFixed());
 // case thuc te
 console.log('case thuc te:', caseNumber.toFixed(3));
+
+// ban chat number deu la floating point
+console.log('23.0 === 23:', 23.0 === 23);
+
+// convert string to number
+console.log(+'23');
+console.log(Number('23'));
+console.log(~~'23');
+
+// parsing
+console.log(Number.parseInt('23px'));
+console.log(Number.parseInt('e23', 10));
+
+console.log(Number.parseInt('2.5rem'));
+console.log(Number.parseFloat('2.5rem'));
+
+// best way checking value is number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+
+// Math
+console.log(Math.sqrt(2)); // can bac 2
+console.log(8 ** 1 / 2);
+// max
+console.log(Math.max(1, 11, 22, '34', 11, 33)); // 34
+console.log(Math.max(1, 11, 22, '34px', 11, 33)); // NaN
+
+// random number in 1 -> 6
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+	Math.trunc(Math.random() * (max - min) + 1 + min);
+
+console.log(randomInt(10, 20));
